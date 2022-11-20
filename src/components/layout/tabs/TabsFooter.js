e255175ad3b6d671f3629f6455d454a26e4bdce9
Eslley <rubemeslley@gmail.com>
2022-11-20 09:35:22 -0300

    Estilizando tab ativa

--- src/components/layout/tabs/TabsFooter.js
+++ src/components/layout/tabs/TabsFooter.js
@@ -1,10 +1,16 @@
 import { CalendarMonth, FormatListBulleted, SportsSoccer } from "@mui/icons-material"
 import { BottomNavigation, BottomNavigationAction, Paper } from "@mui/material"
-import { useNavigate } from "react-router-dom"
+import { useEffect } from "react"
+import { useNavigate, useLocation } from "react-router-dom"
 
 function TabsFooter() {
 
   const navigate = useNavigate()
+  const location = useLocation()
+
+  useEffect(()=> {
+    console.log(location)
+  }, [])
 
   return (
     <Paper sx={{  position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
@@ -12,9 +18,9 @@ function TabsFooter() {
         showLabels
         sx={{ span: { color: 'black' }, svg: { fontSize: 32, color: "#984B43" } }}
       >
-        <BottomNavigationAction onClick={() => navigate('/copainfo/jogos-do-dia')} label="Jogos do Dia" icon={<SportsSoccer />} />
-        <BottomNavigationAction onClick={() => navigate('/copainfo/calendario')} label="Calendário" icon={<CalendarMonth />} />
-        <BottomNavigationAction onClick={() => navigate('/copainfo/grupos')} label="Grupos" icon={<FormatListBulleted />} />
+        <BottomNavigationAction sx={{ backgroundColor: location.pathname === '/copainfo/jogos-do-dia' ? '#b6c6cb' : '' }} onClick={() => navigate('/copainfo/jogos-do-dia')} label="Jogos do Dia" icon={<SportsSoccer />} />
+        <BottomNavigationAction sx={{ backgroundColor: location.pathname === '/copainfo/calendario' ? '#b6c6cb' : '' }} onClick={() => navigate('/copainfo/calendario')} label="Calendário" icon={<CalendarMonth />} />
+        <BottomNavigationAction sx={{ backgroundColor: location.pathname === '/copainfo/grupos' ? '#b6c6cb' : '' }} onClick={() => navigate('/copainfo/grupos')} label="Grupos" icon={<FormatListBulleted />} />
       </BottomNavigation>
     </Paper>
   )
