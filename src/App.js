16dfbd7e36ebdb0fa77686ac26fc95135a7c49fa
Eslley <rubemeslley@gmail.com>
2022-11-21 04:32:19 -0300

    Adicionando loading inicial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { Container } from "@mui/material";
+import { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import AlertMessageProvider from "./components/alert/AlertMessageProvider";
 import Navbar from "./components/layout/navbar/Navbar";
 import TabsFooter from "./components/layout/tabs/TabsFooter";
-import LoadingProvider from "./components/loading/LoadingProvider";
+import { useLoader } from "./components/loading/LoadingProvider";
 import Calendar from "./pages/calendar/Calendar";
 import DaysGames from "./pages/days-games/DaysGames";
 import Groups from "./pages/groups/Groups";
@@ -11,24 +11,30 @@ import Home from "./pages/home/Home";
 import MinhaSelecao from "./pages/minha-selecao/MinhaSelecao";
 
 function App() {
+
+  const { startLoader, stopLoader } = useLoader()
+
+  useEffect(() => {
+    startLoader()
+
+    //Waiting request the API
+    setTimeout(() => {
+      stopLoader()
+    }, 3500)
+  }, [])
+
   return (
     <Router>
       <Navbar />
-
       <Container sx={{ mt: '4em', pb: '1em' }}>
-        <LoadingProvider>
-          <AlertMessageProvider>
-            <Routes>
-              <Route path='/copainfo' element={<Home />} />
-              <Route path='/copainfo/favorita' element={<MinhaSelecao />} />
-              <Route path='/copainfo/jogos-do-dia' element={<DaysGames />} />
-              <Route path='/copainfo/calendario' element={<Calendar />} />
-              <Route path='/copainfo/grupos' element={<Groups />} />
-            </Routes>
-          </AlertMessageProvider>
-        </LoadingProvider>
+        <Routes>
+          <Route path='/copainfo' element={<Home />} />
+          <Route path='/copainfo/favorita' element={<MinhaSelecao />} />
+          <Route path='/copainfo/jogos-do-dia' element={<DaysGames />} />
+          <Route path='/copainfo/calendario' element={<Calendar />} />
+          <Route path='/copainfo/grupos' element={<Groups />} />
+        </Routes>
       </Container>
-
       <TabsFooter />
     </Router>
   )
--- src/index.js
+++ src/index.js
@@ -6,10 +6,16 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import AlertMessageProvider from "./components/alert/AlertMessageProvider";
+import LoadingProvider, { useLoader } from "./components/loading/LoadingProvider";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <LoadingProvider>
+      <AlertMessageProvider>
+        <App />
+      </AlertMessageProvider>
+    </LoadingProvider>
   </React.StrictMode>
 );
